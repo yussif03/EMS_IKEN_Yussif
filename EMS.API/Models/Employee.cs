bae3fb675f37acbbc368bcf92d6150e356128c97
Yussif Abdelmaqsoud <yussifabdelmaqsoud2003@gmail.com>
2026-02-09 12:49:35 +0200

    Done Task2

---
+++ EMS.API/Models/Employee.cs
@@ -0,0 +1,15 @@
+﻿namespace EMS.API.Models
+{
+    public class Employee
+    {
+        public int Id { get; set; }
+        public string FullName { get; set; } = string.Empty;
+        public string Email { get; set; } = string.Empty;
+        public string PhoneNumber { get; set; } = string.Empty;
+        public DateTime HireDate { get; set; }
+        public decimal Salary { get; set; }
+        // Foreign Keys
+        public int DepartmentId { get; set; }
+        public int PositionId { get; set; }
+    }
+}
---
+++ EMS.API/Models/Position.cs
@@ -0,0 +1,10 @@
+﻿namespace EMS.API.Models
+{
+    public class Position
+    {
+        public int Id { get; set; }
+        public string Title { get; set; } = string.Empty;
+        public decimal MinSalary { get; set; }
+        public decimal MaxSalary { get; set; }
+    }
+}
