5e0a2fd17e4b8c3fdf6a34f3f994980a45598680
Yussif Abdelmaqsoud <yussifabdelmaqsoud2003@gmail.com>
2026-02-09 14:51:54 +0200

    Done Task 4

---
+++ EMS.API/Data/Interface/IDbConnectionFactory.cs
@@ -0,0 +1,9 @@
+﻿using System.Data;
+
+namespace EMS.API.Data.Interface
+{
+    public interface IDbConnectionFactory
+    {
+        IDbConnection CreateConnection();
+    }
+}
---
+++ EMS.API/Repositories/Implementation/EmployeeRepository.cs
@@ -0,0 +1,37 @@
+﻿using Dapper;
+using EMS.API.Data.Interface;
+using EMS.API.Models;
+using EMS.API.Repositories.Interface;
+
+namespace EMS.API.Repositories.Implementation
+{
+    public class EmployeeRepository : IEmployeeRepository
+    {
+        private readonly IDbConnectionFactory _connectionFactory;
+
+        public EmployeeRepository(IDbConnectionFactory connectionFactory)
+        {
+            _connectionFactory = connectionFactory;
+        }
+
+        public async Task<int> AddAsync(Employee employee)
+        {
+            var sql = @"
+                INSERT INTO Employee (Name, Email, DepartmentId)
+                VALUES (@Name, @Email, @DepartmentId);
+                SELECT CAST(SCOPE_IDENTITY() as int);
+            ";
+
+            using var connection = _connectionFactory.CreateConnection();
+            return await connection.QuerySingleAsync<int>(sql, employee); // dapper automatically inserts the data in database
+        }
+
+        public async Task<IEnumerable<Employee>> GetAllAsync()
+        {
+            var sql = "SELECT * FROM Employee";
+
+            using var connection = _connectionFactory.CreateConnection();
+            return await connection.QueryAsync<Employee>(sql);
+        }
+    }
+}
