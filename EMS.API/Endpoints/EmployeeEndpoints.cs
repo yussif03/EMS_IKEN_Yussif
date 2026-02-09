5eac7b828457d15466eec3a73eb8620e2e143975
Yussif Abdelmaqsoud <yussifabdelmaqsoud2003@gmail.com>
2026-02-09 12:21:34 +0200

    TASK 1 – Create .NET Minimal API Project

---
+++ EMS.API/Endpoints/EmployeeEndpoints.cs
@@ -0,0 +1,16 @@
+﻿namespace EMS.API.Endpoints
+{
+    public static class EmployeeEndpoints
+    {
+        public static RouteGroupBuilder MapEmployeeEndpoints(this RouteGroupBuilder group)
+        {
+            // Default endpoint
+            group.MapGet("/", () => "EMS API is running...");
+
+            // Health Check Endpoint
+            group.MapGet("/health", () => Results.Ok("Ok"));
+
+            return group;
+        }
+    }
+}
--- EMS.API/Program.cs
+++ EMS.API/Program.cs
@@ -1,3 +1,4 @@
+using EMS.API.Endpoints;
 
 namespace EMS.API
 {
@@ -8,17 +9,19 @@ namespace EMS.API
             var builder = WebApplication.CreateBuilder(args);
 
             // Add services to the container.
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen();
 
             builder.Services.AddControllers();
             // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-            builder.Services.AddOpenApi();
 
             var app = builder.Build();
 
             // Configure the HTTP request pipeline.
             if (app.Environment.IsDevelopment())
             {
-                app.MapOpenApi();
+                app.UseSwagger();
+                app.UseSwaggerUI();
             }
 
             app.UseHttpsRedirection();
@@ -27,6 +30,8 @@ namespace EMS.API
 
 
             app.MapControllers();
+
+            app.MapGroup("/api/employees").MapEmployeeEndpoints();
 
             app.Run();
         }
